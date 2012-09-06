f9882d36d629dc168f7fdd8eb34118addf57b497
David Yu <lewen.yu@gmail.com>
2012-09-06 16:23:41 -0400

    code now correct

--- hotelling.py
+++ hotelling.py
@@ -2,52 +2,48 @@ import sys
 
 class HotellingSimulator():
 
-	def __init__(self, num, numPlayers):
-		self.scale = [False for x in range(num)]
-		self.numPlayers = numPlayers
+    def __init__(self, num, numPlayers):
+        self.scale = [False for x in range(num)]
+        self.numPlayers = numPlayers
 
-	def parse(self, line):
-		words = line.split()
-		if words[0] == "u": # query for utility of player p
-			p = int(words[1]) - 1
-			pos = [-1 for x in range(self.numPlayers)]
-			for i in range (self.numPlayers):
-				pos[i] = int(words[i+2]) - 1
+    def parse(self, line):
+        words = line.split()
+        if words[0] == "u": # query for utility of player p
+            p = int(words[1]) - 1
+            pos = [-1 for x in range(self.numPlayers)]
+            for i in range (self.numPlayers):
+                pos[i] = int(words[i+2]) - 1
 
-			return self.utility(p, pos)
+            return self.utility(p, pos)
 
-	def utility(self, p, allPos):
-		total = 0.0	
-		for i in range(len(self.scale)):
-			best = sys.maxint
-			split = 0
-			isP = False
-			for j in range(len(allPos)):
-				diff = abs(i - allPos[j])
-				if diff < best:
-					split = 1
-					best = diff
-					isP = (j == p)
-				elif diff == best:
-					split += 1
-					isP = (j == p)
-			if isP and split > 0: total += 10.0 / float(split)
-		return total
-
-			
-
-
+    def utility(self, p, allPos):
+        total = 0.0 
+        for i in range(len(self.scale)):
+            best = sys.maxint
+            bestC = -1
+            split = 0
+            for j in range(len(allPos)):
+                diff = abs(i - allPos[j])
+                if diff < best:
+                    split = 1
+                    best = diff
+                    bestC = j
+                elif diff == best:
+                    split += 1
+                    if j == p: bestC = j
+            if bestC == p and split > 0: total += 10.0 / float(split)
+        return total
 
 def main():
-	if (len(sys.argv) < 3): sys.exit()
+    if (len(sys.argv) < 3): sys.exit()
 
-	hs = HotellingSimulator(int(sys.argv[1]), int(sys.argv[2]))
+    hs = HotellingSimulator(int(sys.argv[1]), int(sys.argv[2]))
 
-	while 1:
-		line = sys.stdin.readline()
-		if not line: break
-		print(hs.parse(line))
+    while 1:
+        line = sys.stdin.readline()
+        if not line: break
+        print(hs.parse(line))
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- test.py
+++ test.py
@@ -3,13 +3,21 @@ from hotelling import HotellingSimulator
 
 class HotellingTest(unittest.TestCase):
 
-	def testInitialize(self):
-		hs = HotellingSimulator(20, 2)
-		self.assertTrue(len(hs.scale) == 20)
+    def testInitialize(self):
+        hs = HotellingSimulator(20, 2)
+        self.assertTrue(len(hs.scale) == 20)
 
-	def testParse(self):
-		hs = HotellingSimulator(10, 2)
-		self.assertTrue(hs.parse("u 1 1 1") == 50.0)
+    def testParse(self):
+        hs = HotellingSimulator(10, 2)
+        self.assertTrue(hs.parse("u 1 1 1") == 50.0)
+        self.assertTrue(hs.parse("u 2 1 1") == 50.0)
+
+    def testTwoCandidates(self):
+        hs = HotellingSimulator(10, 2)
+        self.assertTrue(hs.parse("u 1 2 8") == 45.0)
+        self.assertTrue(hs.parse("u 2 2 8") == 55.0)
+        self.assertTrue(hs.parse("u 1 8 9") == 80.0)
+        self.assertTrue(hs.parse("u 2 8 9") == 20.0)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
